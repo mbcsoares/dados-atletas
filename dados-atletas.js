aff299b14cb720bc1855cdbc8ae1ab732d9cb4e8
mbcsoares <79293305+mbcsoares@users.noreply.github.com>
2025-12-29 12:59:13 -0300

    Inclusão do arquivo principal do projeto
    Arquivo principal com o código-fonte. Primeira versão.

---
+++ dados-atletas.js
@@ -0,0 +1,94 @@
+class Atleta
+{
+    constructor(nome, idade, peso, altura, notas)
+    {
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+        this.altura = altura;
+        this.notas = notas;
+    }
+    
+    calculaCategoria()
+    {
+        if(this.idade >= 9 && this.idade <= 11) return "Infantil";
+        else if(this.idade >= 12 && this.idade <= 13) return "Juvenil";
+        else if(this.idade >= 14 && this.idade <= 15) return "Intermediário";
+        else if(this.idade >= 16 && this.idade <= 30) return "Adulto";
+        else return "Sem categoria";
+    }
+    
+    calculaIMC()
+    {
+        return (this.peso / (this.altura * this.altura));
+    }
+    
+    calculaMediaValida()
+    {
+        let soma = 0;
+        let i = 0;
+        let notas = this.notas;
+        
+        notas.sort((a, b) => a - b);
+        
+        for(i = 1; i < notas.length - 1; i++)
+        {
+            soma = soma + notas[i];
+        }
+        
+        return (soma / (i - 1));
+    }
+    
+    obtemNomeAtleta()
+    {
+        return "Nome: " + this.nome;
+    }
+    
+    obtemIdadeAtleta()
+    {
+        return "Idade: " + this. idade;
+    }
+    
+    obtemPesoAtleta()
+    {
+        return "Peso: " + this.peso;
+    }
+    
+    obtemAlturaAtleta()
+    {
+        return "Altura: " + this.altura;
+    }
+    
+    obtemNotasAtleta()
+    {
+        return "Notas: " + this.notas;
+    }
+    
+    obtemCategoria()
+    {
+        return "Categoria: " + this.calculaCategoria();
+    }
+    
+    obtemIMC()
+    {
+        return "IMC: " + this.calculaIMC();
+    }
+    
+    obtemMediaValida()
+    {
+        return "Média válida: " + this.calculaMediaValida();
+    }
+}
+
+const atleta = new Atleta("Cesar Abascal", 30, 80, 1.70, [10, 9.34, 8.42, 10, 7.88]);
+
+console.log(atleta.obtemNomeAtleta());
+console.log(atleta.obtemIdadeAtleta());
+console.log(atleta.obtemPesoAtleta());
+console.log(atleta.obtemAlturaAtleta());
+console.log(atleta.obtemNotasAtleta());
+console.log(atleta.obtemCategoria());
+console.log(atleta.obtemIMC());
+console.log(atleta.obtemMediaValida());
+
+
